f1bf62f1bd465cbf99e5c7b74183c0602384a531
Gilad Bergmann <giladberg@gmail.com>
2018-10-06 20:32:04 +0300

    We stated the frist compoments of angular

--- fullmoonAngular/src/app/app.module.ts
+++ fullmoonAngular/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SliderPictureSectionComponent } from './slider-picture-section/slider-picture-section.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderPictureSectionComponent,
+   
+  
+    
   ],
   imports: [
     BrowserModule
---
+++ fullmoonAngular/src/app/slider-picture-section/slider-picture-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-picture-section',
+  templateUrl: './slider-picture-section.component.html',
+  styleUrls: ['./slider-picture-section.component.css']
+})
+export class SliderPictureSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
