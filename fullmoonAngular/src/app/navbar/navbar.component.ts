95afa304768baf4b571e076f1429fc703cf95d16
gilad bergmann <giladberg@gmail.com>
2018-10-18 22:43:32 +0300

    newJquery

--- fullmoonAngular/src/app/navbar/navbar.component.ts
+++ fullmoonAngular/src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +10,34 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(document).ready(function(){
+      $('#btn1').click(function(){
+        alert('button is clicked');
+      })
+    });
+
+     // Menu-toggle button
+
+     $(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+        $("ul.navi").toggleClass(".showing");
+      });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('.menu-icon').addClass('black');
+      }
+
+      else {
+            $('.menu-icon').removeClass('black');
+      }
+})
+
+
   }
 
 }
