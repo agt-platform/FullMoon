379ed45c553aa6f43a8697e386b45926b0307641
Gilad Bergmann <giladberg@gmail.com>
2018-10-26 13:21:12 +0300

    lmost finish componenta

--- fullmoonAngular/src/app/events/events.component.ts
+++ fullmoonAngular/src/app/events/events.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-empty=["","","","","","","","","","",""];
-  constructor() { }
+events=[
+  {
+    nameOfEvent:'full mitzva',
+    descOfEvent:'FullMoon חגיגות בר/בת מצווה באווירת',
+    picSrc:'../assets/img/g1.jpg'
+ },
+ {
+  nameOfEvent:'Full Wedding',
+  descOfEvent:'FullMoon חתונה באווירת',
+  picSrc:'../assets/img/g2.jpg'
+},
+{
+  nameOfEvent:'Full Bachelorette',
+  descOfEvent:'FullMoon מסיבת רווקים/רווקות באווירת',
+  picSrc:'../assets/img/g3.jpg'
+}
+];
+
+
+  constructor() { 
+
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
