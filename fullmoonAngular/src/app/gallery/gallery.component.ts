a1c1c31f356d6493e579f34fb84699d65a40ad9f
tomeraitz <tomeraitz1@gmail.com>
2018-11-18 16:00:05 +0200

    gallery 2

--- fullmoonAngular/src/app/gallery/gallery.component.ts
+++ fullmoonAngular/src/app/gallery/gallery.component.ts
@@ -6,10 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  
+  gallery=[
+    {
+      picSrc:'../../assets/images/g1.jpg'
+   },
 
-  constructor() { }
+   {
+    picSrc:'../../assets/images/g2.jpg'
+   },
 
-  ngOnInit() {
+  {
+  picSrc:'../../assets/images/g3.jpg'
+  },
+
+  {
+    picSrc:'../../assets/images/g4.jpg'
+  },
+
+  {
+    picSrc:'../../assets/images/g5.jpg'
   }
+
+
+   
+  ];
+  left(){
+       
+  } 
+  constructor() { 
+
+  }
+  ngOnInit(){
+    document.addEventListener("DOMContentLoaded", function(e) {
+    var myIndex = 1;
+    var left = 0;
+    slider();
+
+    function slider() {
+        var i;
+        var x = document.getElementsByClassName("img-responsive") as HTMLCollectionOf<HTMLElement>;
+        var button = document.getElementsByClassName("button") as HTMLCollectionOf<HTMLElement>;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none"; 
+          button[i].style.display = "none"; 
+        }
+        x[myIndex - 1].style.display = "block";  
+        button[myIndex - 1].style.display = "block";  
+    }
+  
+    
+  });
+
+  
+  
+  }
+
+  
 
 }
